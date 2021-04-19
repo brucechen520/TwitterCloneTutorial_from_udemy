6a857e558689b1ad6140ce9013085b97bd027d7e
Bruce.Chen <gibe329apt978@gmail.com>
2021-04-19 22:52:02 +0800

    chapter 24 Changing the chat name

--- public/js/chatPage.js
+++ public/js/chatPage.js
@@ -1,8 +1,12 @@
+$(document).ready(() => {
+    $.get(`/api/chats/${chatId}`, (data) => $("#chatName").text(getChatName(data)))
+})
+
 $("#chatNameButton").click(() => {
     var name = $("#chatNameTextbox").val().trim();
     
     $.ajax({
-        url: "/api/chat/" + chatId,
+        url: "/api/chats/" + chatId,
         type: "PUT",
         data: { chatName: name },
         success: (data, status, xhr) => {
