ea7c63999c893804205ec470499ee94469c400de
Bruce.Chen <gibe329apt978@gmail.com>
2021-06-22 22:02:07 +0800

    Use RXJS implement AutoComplete on search page

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,40 @@
-$("#searchBox").keydown((event) => {
-    clearTimeout(timer);
+const { fromEvent } = rxjs
+const { debounceTime, switchMap, map } = rxjs.operators
+
+let searchbox = fromEvent($("#searchBox"), 'keydown');
+let sub = searchbox.pipe(
+    debounceTime(1000),
+    map(e => e)
+).subscribe(x => searchTemplate(x));
+
+function searchTemplate(event) {
     var textbox = $(event.target);
-    var value = textbox.val();
+    var value = textbox.val().trim();
     var searchType = textbox.data().search;
+    if(value === "")
+        $(".resultsContainer").html("");
+    else
+        search(value, searchType);
+}
 
-    timer = setTimeout(() => {
-        value = textbox.val().trim();
+// $("#searchBox").keydown((event) => {
+//     clearTimeout(timer);
+//     var textbox = $(event.target);
+//     var value = textbox.val();
+//     var searchType = textbox.data().search;
 
-        if(value == "") {
-            $(".resultsContainer").html("");
-        }
-        else {
-            search(value, searchType);
-        }
-    }, 1000)
+//     timer = setTimeout(() => {
+//         value = textbox.val().trim();
 
-})
+//         if(value == "") {
+//             $(".resultsContainer").html("");
+//         }
+//         else {
+//             search(value, searchType);
+//         }
+//     }, 1000)
+
+// })
 
 function search(searchTerm, searchType) {
     var url = searchType == "users" ? "/api/users" : "/api/posts"
